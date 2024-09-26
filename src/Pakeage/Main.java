59effb45e1ec9eb8f65277a11b44813a62fafbd3
dnjswldnd-3513 <zheld3513@gmail.com>
2024-09-26 09:54:58 +0900

    BeakJoon 1008

--- src/Pakeage/Main.java
+++ src/Pakeage/Main.java
@@ -1,23 +1,21 @@
 package Pakeage;
 
 import java.util.Scanner;
-
-import java.util.Scanner;
-
 public class Main {
 
 	public Main() {
 		Scanner sc = new Scanner(System.in);
-		int inputA = sc.nextInt();
-		int inputB = sc.nextInt();
-		System.out.println(inputA*inputB);
+		
+		double a = sc.nextDouble();
+		double b= sc.nextDouble();
+		System.out.println(a/b)
+		;
 	}
 	
 	public static void main(String[] args) {
 		new Main();
 	}
 }
-
 
 
 
