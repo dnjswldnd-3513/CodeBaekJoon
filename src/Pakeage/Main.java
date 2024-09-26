2e4530da3a9b2b12e751e3206b182733f20cf2e0
dnjswldnd-3513 <zheld3513@gmail.com>
2024-09-26 09:53:57 +0900

    BaekJoon 10998

--- src/Pakeage/Main.java
+++ src/Pakeage/Main.java
@@ -10,7 +10,7 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int inputA = sc.nextInt();
 		int inputB = sc.nextInt();
-		System.out.println(inputA-inputB);
+		System.out.println(inputA*inputB);
 	}
 	
 	public static void main(String[] args) {
